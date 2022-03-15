dcf230839134feb57fc780cdfdb7124c5c4f93da
= <=tuanbuinov09@gmail.com>
2022-03-15 10:32:24 +0700

    main form, login form

---
+++ TN_CSDLPT_NOV09/FormMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TN_CSDLPT_NOV09.views;
+namespace TN_CSDLPT_NOV09
+{
+    public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Form frm = this.CheckExists(typeof(FormDangNhap));
+            if (frm != null) frm.Activate();
+            else
+            {
+                FormDangNhap f = new FormDangNhap();
+                f.MdiParent = this;
+                f.Show();
+            }
+        }
+
+        private Form CheckExists(Type ftype)
+        {
+            foreach (Form f in this.MdiChildren)
+                if (f.GetType() == ftype)
+                    return f;
+            return null;
+        }
+
+        private void barButtonDangNhap_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Form frm = this.CheckExists(typeof(FormDangNhap));
+            if (frm != null) frm.Activate();
+            else
+            {
+                FormDangNhap f = new FormDangNhap();
+                f.MdiParent = this;
+                f.Show();
+            }
+        }
+    }
+}
---
+++ TN_CSDLPT_NOV09/views/FormDangNhap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Data.SqlClient;
+
+namespace TN_CSDLPT_NOV09.views
+{
+    public partial class FormDangNhap : DevExpress.XtraEditors.XtraForm
+    {
+        private SqlConnection conn_publisher = new SqlConnection();
+
+        public FormDangNhap()
+        {
+            InitializeComponent();
+
+            
+        }
+        private void FormDangNhap_Load(object sender, EventArgs e)
+        {
+            if (KetNoiCSDLGoc() == 0)
+            {
+                return;
+            }
+            LayDanhSachPhanManh("SELECT * FROM VIEW_DS_COSO");
+            comboBoxCoSo.SelectedIndex = 0;
+        }
+
+        private void LayDanhSachPhanManh(String cmd)
+        {
+            DataTable dt = new DataTable();
+            if (conn_publisher.State == ConnectionState.Closed)
+            {
+                conn_publisher.Open();
+            }
+            SqlDataAdapter sda = new SqlDataAdapter(cmd, conn_publisher);
+            sda.Fill(dt);
+            conn_publisher.Close();
+            Program.bds_DanhSachPhanManh.DataSource = dt;
+            comboBoxCoSo.DataSource = Program.bds_DanhSachPhanManh;
+            comboBoxCoSo.DisplayMember = "TENCS"; comboBoxCoSo.ValueMember = "TENSERVER";
+        }
+
+        private int KetNoiCSDLGoc()
+        {
+            if (conn_publisher != null && conn_publisher.State == ConnectionState.Open)
+            {
+                conn_publisher.Close();
+            }
+            try
+            {
+                conn_publisher.ConnectionString = Program.connstr_publisher;
+                conn_publisher.Open();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Lỗi kết nối cơ sở dữ liệu.\nBạn xem lại user name và password.\n " + e.Message, "", MessageBoxButtons.OK);
+            }
+            return 0;
+        }
+        private void labelBaoLoi_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+    }
+}
